05bb1edc04dba949b54dd680c9e879e3db1a7d16
Ramwh <vasilkovd_67@mail.ru>
2022-10-20 22:24:29 +0400

    Добавлено решение третьей задачи

---
+++ DZ2.3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HomeTasks_KisEA
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            void ifWeekend(int weekday)
+            {
+                if (weekday >= 1 && weekday <=5)
+                   Console.WriteLine("Будний день");
+                else if (weekday == 6 || weekday == 7)
+                    Console.WriteLine("Выходной!");
+                else Console.WriteLine("Такого дня не существует, введите другое число");
+            }
+
+            Console.Write("Введите число соответствующее дню недели ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            ifWeekend(num);
+        }
+    }
+}
