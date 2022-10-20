cb6e0837f1336238741cb40d8309709209ebe191
Ramwh <vasilkovd_67@mail.ru>
2022-10-20 22:04:18 +0400

    Добавлено решение второй задачи

---
+++ DZ2.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HomeTasks_KisEA
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            void printThirdDigit()
+            {
+                Console.Write("Введите трёхзначное число ");
+                int num = Convert.ToInt32(Console.ReadLine());
+                num = Math.Abs(num);
+
+                if ((int)Math.Log10(num) < 2)
+                {
+                    Console.WriteLine("Третьей цифры нет");
+                }
+                else
+                {
+                    string s = Convert.ToString(num);
+                    Console.WriteLine($"Третья цифра заданного числа {s[2]}");
+                }
+            }
+
+            printThirdDigit();
+        }
+    }
+}
